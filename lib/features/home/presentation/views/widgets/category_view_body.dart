6d785a6bdaef82a46ad52b91b70abc076befa665
aliNassef <anassef798@gmail.com>
2024-03-02 00:11:54 +0200

    .

--- lib/features/home/presentation/views/widgets/category_view_body.dart
+++ lib/features/home/presentation/views/widgets/category_view_body.dart
@@ -2,7 +2,6 @@ import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../../../../core/utils/styles.dart';
 import 'custom_app_bar.dart';
 import 'custom_category_list_view_for_choosen.dart';
