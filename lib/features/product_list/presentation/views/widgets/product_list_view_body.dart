d475ccb0710ecb313c2e09b41687d0620a91b354
aliNassef <anassef798@gmail.com>
2024-03-02 11:32:36 +0200

    create product list view body

---
+++ lib/features/product_list/presentation/views/widgets/product_list_view_body.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../home/presentation/views/widgets/custom_app_bar.dart';
+import 'custom_product.dart';
+
+class ProductListViewBody extends StatelessWidget {
+  const ProductListViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const CustomAppBar(),
+        SizedBox(
+          height: 24.h,
+        ),
+        Expanded(
+          child: GridView.builder(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 180 / 273,
+              mainAxisSpacing: 16.h,
+              crossAxisSpacing: 16.w,
+            ),
+            itemBuilder: (context, index) {
+              return const CustomProduct();
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
