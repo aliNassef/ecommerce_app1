5d92fd9eebdc6d6788c347c9a0c3885df4f6f33c
aliNassef <anassef798@gmail.com>
2024-03-02 10:29:22 +0200

    .

--- lib/core/api/server_locator.dart
+++ lib/core/api/server_locator.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:ecommerce_app/core/api/dio_consumer.dart';
 import 'package:ecommerce_app/features/home/data/repos/home_repo/home_repo_impl.dart';
 import 'package:get_it/get_it.dart';
-
 import '../../features/auth/data/repo/auth_repo_impl.dart';
 
 final GetIt getIt = GetIt.instance;
