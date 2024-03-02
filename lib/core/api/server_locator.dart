b4dea0d112df1a3ee6d8d02fbe3556a047c2f52e
aliNassef <anassef798@gmail.com>
2024-03-02 07:34:27 +0200

    done

--- lib/core/api/server_locator.dart
+++ lib/core/api/server_locator.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:ecommerce_app/core/api/dio_consumer.dart';
 import 'package:ecommerce_app/features/home/data/repos/home_repo/home_repo_impl.dart';
-import 'package:ecommerce_app/features/home/data/repos/sub_category_repo/sub_category_repo_impl.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../features/auth/data/repo/auth_repo_impl.dart';
@@ -21,13 +20,6 @@ void setupGetIt() {
   );
   getIt.registerSingleton<HomeRepoImpl>(
     HomeRepoImpl(
-      api: DioConsumer(
-        dio: Dio(),
-      ),
-    ),
-  );
-  getIt.registerSingleton<SubCategoryRepoImpl>(
-    SubCategoryRepoImpl(
       api: DioConsumer(
         dio: Dio(),
       ),
--- lib/features/home/data/repos/sub_category_repo/sub_category_repo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import '../../models/sub_category_model/datum.dart';
-
-abstract class SubCategoryRepo {
-  Future<Either<List<Datum>, String>> fetchAllSubCategories();
-}
