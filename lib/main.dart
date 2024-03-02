b8be40c7cd2092dc957824ffafe991b1817e47e3
aliNassef <anassef798@gmail.com>
2024-03-02 07:31:25 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'core/cache/cache_helper.dart';
 import 'features/auth/data/repo/auth_repo_impl.dart';
 import 'features/auth/presentation/manger/authantication/authantication_cubit.dart';
-import 'features/home/data/repos/sub_category_repo/sub_category_repo_impl.dart';
-
+ 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   CacheHelper().init();
