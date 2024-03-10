7131e6fd541f44312b75e0e85354c80882aa9df3
aliNassef <anassef798@gmail.com>
2024-03-10 12:00:16 +0200

    extract widget

--- lib/features/cart/presentation/views/widgets/cart_view_body.dart
+++ lib/features/cart/presentation/views/widgets/cart_view_body.dart
@@ -1,12 +1,12 @@
 import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/features/cart/presentation/manger/cart_cubit/cart_cubit.dart';
-import 'package:ecommerce_app/features/favorite/presentation/views/favorite_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../../core/utils/styles.dart';
 import 'cart_item.dart';
+import 'check_out_button.dart';
 
 class CartViewBody extends StatefulWidget {
   const CartViewBody({super.key});
@@ -63,48 +63,7 @@ class _CartViewBodyState extends State<CartViewBody> {
                       SizedBox(
                         width: 33.w,
                       ),
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () async {},
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: kPrimaryColor,
-                            ),
-                            height: 48.h,
-                            child: Center(
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 16,
-                                ),
-                                child: Row(
-                                  children: [
-                                    SizedBox(
-                                      width: 24.w,
-                                    ),
-                                    Text(
-                                      'Check Out',
-                                      style: TextStyle(
-                                        fontSize: 20.sp,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 24.w,
-                                    ),
-                                    const Icon(
-                                      Icons.trending_flat,
-                                      color: Colors.white,
-                                      size: 24,
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
+                      const CheckOutButton(),
                     ],
                   ),
                   SizedBox(
