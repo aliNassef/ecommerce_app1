cfb56920b63a9f3781ed6d572356bb3d0c4fabb0
aliNassef <anassef798@gmail.com>
2024-03-12 11:43:05 +0200

    finish get fav function

--- lib/features/favorite/presentation/manger/cubit/fav_cubit.dart
+++ lib/features/favorite/presentation/manger/cubit/fav_cubit.dart
@@ -1,8 +1,20 @@
-import 'package:bloc/bloc.dart';
+import 'package:ecommerce_app/features/favorite/data/models/fav_model/fav_model.dart';
+import 'package:ecommerce_app/features/favorite/data/repo/fav_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'fav_state.dart';
 
 class FavCubit extends Cubit<FavState> {
-  FavCubit() : super(FavInitial());
+  FavCubit(this.favRepo) : super(FavInitial());
+  final FavRepo favRepo;
+
+  getFavProducts({required String id}) async {
+    emit(FavLoading());
+    final result = await favRepo.getFavProducts(id: id);
+    result.fold(
+      (l) => emit(FavLoaded(favModel: l)),
+      (r) => emit(FavFailure(errMessage: r)),
+    );
+  }
 }
--- lib/features/favorite/presentation/manger/cubit/fav_state.dart
+++ lib/features/favorite/presentation/manger/cubit/fav_state.dart
@@ -8,3 +8,17 @@ sealed class FavState extends Equatable {
 }
 
 final class FavInitial extends FavState {}
+
+final class FavLoading extends FavState {}
+
+final class FavLoaded extends FavState {
+  final FavModel favModel;
+
+  const FavLoaded({required this.favModel});
+}
+
+final class FavFailure extends FavState {
+  final String errMessage;
+
+  const FavFailure({required this.errMessage});
+}
